e3d7640d878fbec1b16f9aef845db787d3b075fb
Joren <Joren.dom@outlook.com>
2022-03-26 14:44:12 +0100

    add functionality and make the game work

--- script.js
+++ script.js
@@ -20,49 +20,75 @@ function run(){
 
 
 
-function computerPlay(){
-    const hands = ["Rock","Paper","Scissors"];
-    computerPlay = Math.floor(Math.random()*3);
-    return hands[computerPlay];
-}
+
 
 
 function playRound(playerSelection, computerSelection){
+   let computerScore = 0;
+   let playerScore = 0;
     
-    if(playerSelection ==="Rock" && computerSelection == "Scissors")
-    {
-        console.log("Player won with Rock!");
-    }
-    else if(playerSelection ==="Scissors" && computerSelection == "Paper")
-    {
-        console.log("Player won with scissors!!");
-    }
-    else if(playerSelection === "Paper" && computerSelection == "Rock")
-    {
-        console.log("Player won with Paper!");
-    }
-    else if(playerSelection === computerSelection){
-        console.log("Draw")
-    }
-    else{
-        console.log("Computer won!");
-    }
+     if(playerSelection === computerSelection){
+         console.log("Tie");
+     }
+     else if(playerSelection == "Rock"){
+         if(computerSelection == "Paper"){
+             console.log("Computer wins!");
+             computerScore++;
+             console.log(`CPU score: ${computerScore}`)
+         }
+
+         else if(computerSelection == "Scissors"){
+            console.log("Player wins!");
+            playerScore++;
+            console.log(`Player score: ${playerScore}`)
+         }
+     }
+     else if(playerSelection == "Paper"){
+         if(computerSelection == "Scissors"){
+             console.log("Computer wins!")
+             computerScore++;
+             console.log(`CPU score: ${computerScore}`)
+         }
+         else if(computerSelection == "Rock"){
+             console.log("Player wins!")
+             playerScore++;
+             console.log(`Player score: ${playerScore}`)
+
+         }
+     }
+     else if(playerSelection == "Scissors"){
+         if(computerSelection == "Rock"){
+             console.log("Computer wins!")
+             computerScore++;
+             console.log(`CPU score: ${computerScore}`)
+         }
+         else if(computerSelection == "Paper"){
+             console.log("Player wins!")
+             playerScore++;
+             console.log(`Player score: ${playerScore}`)
+         }
+     }
 }
-const computerSelection = computerPlay();
-function game(){
-    for(let i = 0; i < 5; i++){
-        
-        
-        const playerSelection = prompt("Rock Paper or Scissors");
-        
-        playRound(playerSelection, computerSelection);
-        console.log(playRound())
-
-    }
+function computerPlay(){
+    const choice = ["Rock","Paper","Scissors"];
+    x = Math.floor(Math.random()*3);
+    return choice[x];
 }
 
 
+    function game(playerScore, computerScore){
+        for(let i = 0; i < 5; i++){
+            
+            const computerSelection = computerPlay();
+            const playerSelection = prompt("Rock Paper or Scissors");
+            
+            
+            playRound(playerSelection, computerSelection);
+            console.log(playerSelection);
+            console.log(computerSelection);
+            
 
+        }
+    }
 
-
-//console.log(playRound(playerSelection, computerSelection));
+game();
